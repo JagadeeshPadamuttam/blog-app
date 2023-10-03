f78e4ef7b5eb5d45086e43963e471df5fcb288d6
JAGADEESH PADAMUTTAM <jagadeeshpadamuttam807@gmail.com>
2023-10-03 12:42:10 +0530

    change in server

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const app=express();
 
 //middleware
 app.use(cors({
-  origin:["https://mern-blog-rr8b.onrender.com"],
+  origin:["https://blog-app-apz1.onrender.com"],
   methods:["POST","GET"],
   credentials:true
   }));
